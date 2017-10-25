38d33be410b489765b37ed2fb522126831a8d4b7
Jesse Benson <benson.jesse@gmail.com>
2017-10-24 20:55:25 -0700

    Add serilog configuration.

---
+++ src/Common/AppConfig.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Common
+{
+	public static class AppConfig
+	{
+		public static Uri ElasticsearchUri => new Uri(ElasticsearchUrl);
+		public static string ElasticsearchUrl => "";
+		public static string ElasticsearchUsername => "";
+		public static string ElasticsearchPassword => "";
+	}
+}
