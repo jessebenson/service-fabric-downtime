e891bd1872f7cbeee374ca79d9b44812defe2e02
jessebenson <jesse.benson@microsoft.com>
2017-10-25 11:12:36 -0700

    Handle Service Fabric failover-related exceptions, and allow Reverse Proxy to re-resolve the primary.

--- src/StatefulSvc/Controllers/CountController.cs
+++ src/StatefulSvc/Controllers/CountController.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 using Serilog;
 using System;
 using System.Diagnostics;
+using System.Fabric;
 using System.Net;
 using System.Threading;
 using System.Threading.Tasks;
@@ -31,15 +32,36 @@ namespace StatefulSvc.Controllers
 			{
 				long value = await _statefulSvc.IncrementAsync(token).ConfigureAwait(false);
 
-				_logger.Information("{MethodName} completed in {ElapsedTime} ms. {CorrelationId}", "api/count/reverse-proxy", timer.ElapsedMilliseconds, correlationId);
+				_logger.Information("{MethodName} completed with {StatusCode} in {ElapsedTime} ms. {CorrelationId}", "api/count/reverse-proxy", (int)HttpStatusCode.OK, timer.ElapsedMilliseconds, correlationId);
 
 				return Ok(value);
 			}
 			catch (Exception e)
 			{
-				_logger.Error(e, "{MethodName} failed in {ElapsedTime} ms. {CorrelationId}", "api/count/reverse-proxy", timer.ElapsedMilliseconds, correlationId);
-				return StatusCode((int)HttpStatusCode.InternalServerError, e.Message);
+				// NotFound will allow Service Fabric's reverse proxy to re-resolve the new primary.
+				var status = IsFailoverException(e) ? HttpStatusCode.NotFound : HttpStatusCode.InternalServerError;
+
+				_logger.Error(e, "{MethodName} failed with {StatusCode} in {ElapsedTime} ms. {CorrelationId}", "api/count/reverse-proxy", (int)status, timer.ElapsedMilliseconds, correlationId);
+				return StatusCode((int)status, e.Message);
 			}
+		}
+
+		private static bool IsFailoverException(Exception e)
+		{
+			if (e is FabricNotPrimaryException || e is FabricNotReadableException)
+				return true;
+
+			var agg = e as AggregateException;
+			if (agg != null)
+			{
+				foreach (var ex in agg.Flatten().InnerExceptions)
+				{
+					if (ex is FabricNotPrimaryException || ex is FabricNotReadableException)
+						return true;
+				}
+			}
+
+			return false;
 		}
 	}
 }
